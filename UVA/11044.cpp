23237d9bf2107118f3610c082a9b0e291fa59693
Samuel Torres <30709501+SamuelT13@users.noreply.github.com>
2017-08-03 23:44:19 -0400

    Update format

--- UVA/11044.cpp
+++ UVA/11044.cpp
@@ -9,8 +9,8 @@ int main()
 	cin >> test;
 	
 	while(test-- > 0){
-	cin >> x >> y;
-	cout << (int)(ceil((x-2)/3.0)*ceil((y-2)/3.0)) << endl;
+		cin >> x >> y;
+		cout << (int)(ceil((x-2)/3.0)*ceil((y-2)/3.0)) << endl;
 	}
 	
 	return 0;

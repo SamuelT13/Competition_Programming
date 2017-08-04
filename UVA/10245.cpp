47437321ea7f26b6285974a1a438da9e8ae45e7c
Samuel Torres <30709501+SamuelT13@users.noreply.github.com>
2017-08-03 23:40:41 -0400

    Update format

--- UVA/10245.cpp
+++ UVA/10245.cpp
@@ -50,9 +50,9 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-    cout.setf(ios::fixed);
-    cout.setf(ios::showpoint);
-    cout.precision(4);
+    	cout.setf(ios::fixed);
+    	cout.setf(ios::showpoint);
+    	cout.precision(4);
     
 	while (cin >> total && total){
 		P.clear(); P.assign(total, point(0, 0));

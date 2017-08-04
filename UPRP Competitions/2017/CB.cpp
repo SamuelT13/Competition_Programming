477d74f8a303daf3772e14a0f4b0b5369964a7a9
Samuel Torres <30709501+SamuelT13@users.noreply.github.com>
2017-08-03 23:34:54 -0400

    Update format

--- UPRP Competitions/2017/CB.cpp
+++ UPRP Competitions/2017/CB.cpp
@@ -1,27 +1,3 @@
-/*
-Breanna was bored. The power had gone out in her neighborhood (again). No power, no router, no internet. 
-She had taken to playing paper-and-pencil games with her brother to pass the time. Even that could only entertain them for so long. 
-Suddenly, her brother wrote down a sequence of integers.
-"If you could write each of these numbers once to combine them into a larger one, what is the greatest number you could form," he asked. 
-Maybe he could distract her for a while. They started with the numbers 1, 2, 3, 4, 5 through. 
-Breanna could easily see that the answer was 54321. But the questions became tougher as the sequences became longer. Could you solve these puzzles too?
-*/
-
-/*
-Input Format
-
-The input will begin with a line containing an integer T, denoting the number of test cases. 
-Each case will consist of two integers A and B, representing the sequence of integers from A to B. There will be a space separating those values.
-*/
-
-/*
-Output Format
-
-The output will consist of one line for each test case. 
-Each line will have the form Case #X: Y, where X indicates the case number and Y indicates the maximum number that can be produced by concatenating the numbers in the given sequence. 
-The order of the results must follow the same order in which the test cases are provided.
-*/
-
 #include <cmath>
 #include <cstdio>
 #include <vector>

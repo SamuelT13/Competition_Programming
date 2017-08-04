b41e4b1b408c65287435e76769c9a7fb72c54989
Samuel Torres <30709501+SamuelT13@users.noreply.github.com>
2017-08-04 11:11:18 -0400

    Update format

--- Algorithms/SubsetSum.cpp
+++ Algorithms/SubsetSum.cpp
@@ -13,12 +13,12 @@ ifstream in("datos.txt"); ofstream out("sumas.txt");
  
 // prints subset found
 void printSubset(vector<int>& data, vector<int> t, int t_size){
-    for(int i = 0; i < t_size; i++)
-        if (t[i]){
+	for (int i = 0; i < t_size; i++)
+        	if (t[i]){
 			*find(data.begin(), data.end(), t[i]) = 0;
 			out << "$" << t[i]/100 << "." << setw(2) << setfill('0') << t[i]%100 << "\n";
-    	}    
-    out << endl;
+    		}    
+    	out << endl;
 }
  
 bool f(int d1, int d2){
@@ -26,43 +26,44 @@ bool f(int d1, int d2){
 }
 
 bool subset_sum(vector<int>& s, vector<int> t, int s_size, int t_size, int sum, int ite, int const target_sum){ 
-    if( target_sum == sum ){
-        // We found sum
-        out << "Target Sum found:\n";
-        printSubset(s, t, t_size);
+    	if (target_sum == sum){
+        	// We found sum
+        	out << "Target Sum found:\n";
+        	printSubset(s, t, t_size);
 		return true;
-    }
+    	}
     
-    else{
-		if(ite < (int)s_size && sum + s[ite] <= target_sum) // generate nodes along the breadth
-            for(int i = ite; i < (int)s_size; i++){
-				t[t_size] = s[i];
+    	else{
+		if (ite < (int)s_size && sum + s[ite] <= target_sum) // generate nodes along the breadth
+            	for (int i = ite; i < (int)s_size; i++){
+			t[t_size] = s[i];
 
-                if( sum + s[i] <= target_sum ) if (subset_sum(s, t, s.size(), t_size + 1, sum + s[i], i + 1, target_sum)) return true;
+                	if (sum + s[i] <= target_sum) 
+				if (subset_sum(s, t, s.size(), t_size + 1, sum + s[i], i + 1, target_sum)) return true;
                 // consider next level node (along depth)
-            }
-    }
+            	}
+    	}
     
     return false;
 }
  
 // Wrapper that prints subsets that sum to target_sum
 void generateSubsets(vector<int>& s, int target_sum){
-    vector<int> tuplet_vector(s.size());
+	vector<int> tuplet_vector(s.size());
  
-    int total = 0;
+    	int total(0);
  
-    // sort the set
-    sort(s.begin(), s.end());
+    	// sort the set
+    	sort(s.begin(), s.end());
 	 
-    for( int i = 0; i < (int)s.size(); i++) total += s[i];
+    	for (int i = 0; i < (int)s.size(); i++) total += s[i];
  	
-    if(s[0] <= target_sum && total >= target_sum){
+    	if (s[0] <= target_sum && total >= target_sum){
 		if (!subset_sum(s, tuplet_vector, s.size(), 0, 0, 0, target_sum)) out << "No matches found!\n\n";
 	}
 	else out << "No matches found!\n\n";
 	
-    tuplet_vector.clear();
+    	tuplet_vector.clear();
 }
  
 struct info{
@@ -71,8 +72,8 @@ struct info{
 };
 
 int main(){
-    vector<int> data;
-    vector<info> total, needed;
+    	vector<int> data;
+    	vector<info> total, needed;
 	int amount, target;
 	info temp;
 	int month, day, s, t, j, k, mult, num;
